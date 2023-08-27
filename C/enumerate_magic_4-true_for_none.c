cfc1fb22e5593d3d3a05bbbf7595519339e5c921
TounSec <hackethique@gmail.com>
2023-08-27 22:28:28 +0200

    New

---
+++ C/enumerate_magic_4-true_for_none.c
@@ -0,0 +1,18 @@
+/*
+Create a method none? (JS none) that accepts an array and a block (JS: a function), and returns true if the block (/function) returns true for none of the items in the array. An empty list should return true.
+*/
+#include <stdio.h>
+#include <stdbool.h>
+#include <stddef.h>
+
+typedef bool (*Predicate)(int);
+
+bool none(const int* arr, size_t size, Predicate func)
+{
+    for (size_t i = 0; i < size; i++) {
+        if (func(arr[i]))
+            return false;
+    }
+
+    return true;
+}
